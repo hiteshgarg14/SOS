5718df1b839628945e8a48e87a67b61040a63a93
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 02:22:50 +0530

    staticfiles configured accordind to heroku

--- sos/settings/production.py
+++ sos/settings/production.py
@@ -2,7 +2,7 @@ from common import *
 import psycopg2
 import urlparse
 
-
+"""
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
@@ -11,9 +11,23 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+"""
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+#TEMPLATE_DIR = os.path.join(BASE_DIR, '../templates')
+STATIC_DIR = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../collectstatic')
+
+STATICFILES_DIRS = [STATIC_DIR,]
+
+
+STATIC_URL = '/collectstatic/'
+MEDIA_URL = '/media/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #To be updated
 ALLOWED_HOSTS = ['*']
