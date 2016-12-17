3958c0ecfc57be08d15a92fe8318da6b15107735
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 01:40:12 +0530

    staticfiles

--- sos/settings/common.py
+++ sos/settings/common.py
@@ -151,5 +151,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/collectstatic/'
 MEDIA_URL = '/media/'
