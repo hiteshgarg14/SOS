65fca85607ad391c4e24e2866305a0f42c8a4e2d
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 00:31:58 +0530

    database link problem solved

--- sos/settings/production.py
+++ sos/settings/production.py
@@ -19,11 +19,11 @@ url = urlparse.urlparse(os.environ["DATABASE_URL"])
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'url.path[1:]',
-        'USER': 'url.username',
-        'PASSWORD': 'url.password',
-        'HOST': 'url.hostname',
-        'PORT': 'url.port',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
     }
 }
 
