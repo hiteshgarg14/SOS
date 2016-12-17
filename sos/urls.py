b51fb639033b0bf6d7a587e2c69a46c0a3aa3e7a
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 01:59:25 +0530

    static files

--- sos/urls.py
+++ sos/urls.py
@@ -22,7 +22,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^', include('sosapp.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
