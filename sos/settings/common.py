df9e899647bcd55bd34ecf54d89d31540cd98a0b
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 02:14:34 +0530

    staticfiles configured accordind to heroku

--- sos/settings/common.py
+++ sos/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = MEDIA_DIR
 TEMPLATE_DIR = os.path.join(BASE_DIR, '../templates')
