e23a52e8d789c6fed5c4e9fca53d06150561d472
Hitesh Garg <garg95hitesh@gmail.com>
2016-12-18 01:49:19 +0530

    cling added

--- sos/urls.py
+++ sos/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     url(r'^', include('sosapp.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
